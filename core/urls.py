56b251c62497701810abeae9b8dea85e3a1196ce
Algie Parvin <pauchok97@gmail.com>
2018-03-09 19:20:34 +0200

    Added paganation

--- core/urls.py
+++ core/urls.py
@@ -2,9 +2,19 @@ from django.urls import path, re_path
 
 from core import views
 
+
+REDDIT = 'reddit'
+ML_MASTERY = 'Machine Learning Mastery'
+
+
 urlpatterns = [
-    path('reddit/', views.reddit),
-    path('ml_mastery/', views.machine_learning_mastery),
-    re_path(r'ml_mastery/(?P<period>\w{0,50})/', views.machine_learning_mastery, name='Machine Learning Mastery'),
-    re_path(r'reddit/(?P<period>\w{0,50})/', views.reddit, name='reddit'),
+    path('', views.reddit),
+    path('reddit/', views.reddit, name=REDDIT),
+    path('ml_mastery/', views.machine_learning_mastery, name=ML_MASTERY),
+
+    re_path(r'^ml_mastery/(?P<period>\w{0,50})/$', views.machine_learning_mastery, name=ML_MASTERY),
+    re_path(r'^reddit/(?P<period>\w{0,50})/$', views.reddit, name=REDDIT),
+
+    re_path(r'^ml_mastery/(?P<period>\w{0,50})/(?P<page>\d+)/$', views.machine_learning_mastery, name=ML_MASTERY),
+    re_path(r'^reddit/(?P<period>\w{0,50})/(?P<page>\d+)/$', views.reddit, name=REDDIT),
 ]
--- core/views.py
+++ core/views.py
@@ -2,16 +2,22 @@ from datetime import datetime, timedelta
 
 from django.http import Http404
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 from core.models import Article, Site
 
 
-def machine_learning_mastery(request, period='all'):
-    return get_articles(request, 'Machine Learning Mastery', period)
+PERIODS = ['all', 'today', 'yesterday', 'week', 'month']
+ARTICLES_PER_PAGE = 20
+PAGES_TO_DISPLAY = 10
 
 
-def reddit(request, period='all'):
-    return get_articles(request, 'reddit', period)
+def machine_learning_mastery(request, period=PERIODS[0], page=1):
+    return get_articles(request, 'Machine Learning Mastery', period, page)
+
+
+def reddit(request, period=PERIODS[0], page=1):
+    return get_articles(request, 'reddit', period, page)
 
 
 def load_articles(site_name, period):
@@ -44,12 +50,37 @@ def load_articles(site_name, period):
     raise Http404('Page not found')
 
 
-def get_articles(request, site_name, period='all'):
-    articles = load_articles(site_name, period)
+def get_articles(request, site_name, period, page):
+
+    # add try / catch
+    page_index = int(page)
+
+    paginator = Paginator(load_articles(site_name, period), ARTICLES_PER_PAGE)
+    articles = paginator.page(page)
+
+    start_page = page_index - (PAGES_TO_DISPLAY - 1) \
+        if page_index % PAGES_TO_DISPLAY == 0 \
+        else page_index - (page_index % PAGES_TO_DISPLAY) + 1
+    end_page = min(start_page + PAGES_TO_DISPLAY - 1, paginator.num_pages)
+
+    pages = list(range(start_page, end_page + 1))
+
     sites = Site.objects.all()
     current_site = Site.objects.get(name=site_name)
-    periods = ['all', 'today', 'yesterday', 'week', 'month']
-    context = {'articles': articles, 'sites': sites,
-               'current_site': current_site, 'periods': periods,
-               'period': period}
+
+    context = {'articles': articles,
+               'sites': sites,
+               'current_site': current_site,
+
+               'periods': PERIODS,
+               'period': period,
+
+               'page': page_index,
+               'pages': pages,
+               'next_page_to_scroll': end_page + 1,
+               'previous_page_to_scroll': start_page - 1,
+
+               'has_previous_pages': start_page != 1,
+               'has_next_pages': end_page < paginator.num_pages,
+               'no_pagination': paginator.num_pages == 1}
     return render(request, 'core/articles.html', context)
